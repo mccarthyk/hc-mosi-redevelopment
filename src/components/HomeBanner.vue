9de765d11a3c01ff08fb8834ca7bd15edda622f8
Kevin McCarthy <mccarthyk@hcflgov.net>
2020-12-04 14:22:21 -0500

    project cancelation

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -5,9 +5,30 @@
       <div class="py-5 my-5 text-center">
         <!-- <h1 class="display-1 font-weight-bold text-dark">Develop in the Center of Inovation</h1> -->
         <!-- <h2 class="display-2 font-weight-bold">Hillsborough County Seeks Partner to Redevelop MOSI Complex</h2> -->
-        <h2
-          class="display-3 font-weight-bold"
-        >Hillsborough County Seeks Master Developer to Redevelop 74-acre site in the heart of Tampa Bay's Innovation District</h2>
+        <h2 class="display-3 font-weight-bold">
+          Hillsborough County Seeks Master Developer to Redevelop 74-acre site
+          in the heart of Tampa Bay's Innovation District
+        </h2>
+      </div>
+
+      <div class="my-5">
+        <p class="lead text-danger">
+          <strong
+            >The County has decided to cancel the solicitation for redevelopment
+            of the Museum of Science and Industry property</strong
+          >
+          (20273,3). Given the widespread and sustained impact of the COVID-19
+          Pandemic and numerous other issues related to the same, not the least
+          of which is a recent and rapid increase in cases, there is significant
+          uncertainty about the current and future state of our local economy.
+          It is absolutely necessary to reconsider this project within the
+          context of current events, the community’s need, and the Board’s
+          priorities over the next several years. To maintain the integrity of
+          the process and protect interested proposers, cancelling the
+          solicitation before proposals are received is both prudent and
+          appropriate. We sincerely appreciate your kind patience and
+          consideration in this matter.
+        </p>
       </div>
 
       <hr />
@@ -16,11 +37,23 @@
       <div class="d-md-flex align-items-center">
         <div class="col-md-8">
           <div class="lead">
-            <p>Hillsborough County has a historic opportunity to redevelop the site of the Museum of Science and Industry (MOSI) at 4801 Fowler Ave, Tampa, Florida. The County seeks a visionary development partner who will leverage the innovation that surrounds the site and the area’s booming economy to create an urban, mixed-use hub for cutting-edge companies, sustainable living and community advancement.</p>
+            <p>
+              Hillsborough County has a historic opportunity to redevelop the
+              site of the Museum of Science and Industry (MOSI) at 4801 Fowler
+              Ave, Tampa, Florida. The County seeks a visionary development
+              partner who will leverage the innovation that surrounds the site
+              and the area’s booming economy to create an urban, mixed-use hub
+              for cutting-edge companies, sustainable living and community
+              advancement.
+            </p>
           </div>
         </div>
         <div class="col-md-4">
-          <img src="@/assets/undraw_urban_design_kpu8.svg" alt class="img-responsive img-fluid" />
+          <img
+            src="@/assets/undraw_urban_design_kpu8.svg"
+            alt
+            class="img-responsive img-fluid"
+          />
         </div>
       </div>
 
@@ -31,14 +64,16 @@
             href="https://www.hillsboroughcounty.org/library/hillsborough/media-center/documents/procurement-services/rfps/mosi.pdf"
             target="_blank"
             class="btn btn-block btn-outline-dark btn-lg display-4 my-3"
-          >View the RFP</a>
+            >View the RFP</a
+          >
         </div>
         <div class="col-xl-6">
           <a
             href="https://www.hillsboroughcounty.org/en/government/departments/procurement"
             target="_blank"
             class="btn btn-block btn-outline-dark btn-lg display-4 my-3"
-          >Contact Procurement Services</a>
+            >Contact Procurement Services</a
+          >
         </div>
       </div>
 
